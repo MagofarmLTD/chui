67949b332197f427eab06a6279e65d2c426c66d6
tony <b.mboya@alustudent.com>
2023-01-10 11:13:09 +0200

    added the new feedback

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,21 +1,66 @@
 import React from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa/index";
 
 const Nav = () => {
   const [showNav, setShowNav] = React.useState(false);
-  console.log(showNav);
+
   return (
-    <div className="mr-5">
-      {!showNav ? (
-        <button
-          onClick={(showNav) => setShowNav(!showNav)}
-          className="cursor-pointer"
+    <div className="text-white  pr-5 z-[100]">
+      <div
+        className="absolute z-[9999] right-5 cursor-pointer md:hidden"
+        onClick={() => {
+          setShowNav(!showNav);
+        }}
+      >
+        {!showNav ? (
+          <FaBars size={25} fill="#fff" />
+        ) : (
+          <FaTimes size={30} fill="#fff" />
+        )}
+      </div>
+
+      {showNav && (
+        <section
+          className={
+            !showNav
+              ? "hidden"
+              : "absolute top-0 left-0  flex h-[400px] w-full flex-col items-center justify-center bg-green text-lg font-medium text-white bg-blue z-[50] "
+          }
         >
-          <FaBars size={30} color="#fff" />
-        </button>
-      ) : (
-        <div>Hey Biatch</div>
+          <ul className="space-y-5" onClick={() => setShowNav(!showNav)}>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              {" "}
+              <a href="#aboutUs">About Us</a>
+            </li>
+            <li>
+              <a href="/blogs">Blogs</a>
+            </li>
+            <li>
+              <a href="#mawasiliano">Contact Us</a>
+            </li>
+          </ul>
+        </section>
       )}
+
+      {/* For wide Screens */}
+      <ul className="md:flex gap-5 hidden text-lg">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          {" "}
+          <a href="#aboutUs">About Us</a>
+        </li>
+        <li>
+          <a href="/blogs">Blogs</a>
+        </li>
+        <li>
+          <a href="#mawasiliano">Contact Us</a>
+        </li>
+      </ul>
     </div>
   );
 };
